848b185dc4d63da9b3009c1c5d0e598b7b42cf4a
Siradio7 <mamadousiradioudiallo4@gmail.com>
2026-08-19 14:54:46 +0200

    déléguer les routes produits au contrôleur

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,52 +1,11 @@
 import express from "express"
-import { create, findById, updateById, deleteById } from "../services/product.js"
+import { createProduct, findProductById, updateProduct, deleteProduct } from "../controllers/product.js"
 
 const productRouter = express.Router()
 
-productRouter.post("/", async (req, res) => {
-    const productData = req.body
-
-    if (!productData.name || !productData.description || productData.price === undefined) {
-        return res.status(400).json({ error: "Missing required fields" })
-    }
-
-    const productCreated = await create(productData)
-
-    return res.status(201).json(productCreated)
-})
-
-productRouter.get("/:id", async (req, res) => {
-    const { id } = req.params
-    const product = await findById(id)
-
-    if (!product) {
-        return res.status(404).json({ error: "Product not found" })
-    }
-
-    return res.status(200).json(product)
-})
-
-productRouter.put("/:id", async (req, res) => {
-    const { id } = req.params
-    const productData = req.body
-    const isUpdated = await updateById(id, productData)
-
-    if (!isUpdated) {
-        return res.status(404).json({ error: "Product not found" })
-    }
-
-    return res.status(200).json({ message: "Product updated successfully" })
-})
-
-productRouter.delete("/:id", async (req, res) => {
-    const { id } = req.params
-    const isDeleted = await deleteById(id)
-
-    if (!isDeleted) {
-        return res.status(404).json({ error: "Product not found" })
-    }
-
-    return res.status(204).send()
-})
+productRouter.post("/", createProduct)
+productRouter.get("/:id", findProductById)
+productRouter.put("/:id", updateProduct)
+productRouter.delete("/:id", deleteProduct)
 
 export default productRouter
